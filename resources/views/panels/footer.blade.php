e67a9546a91346aebebacb65467e128465b742bc
Jonas Damher <jonas.damher@gmailcom>
2022-02-02 19:40:35 +0000

    ADD: - svg social networks - footer REMOVE: - bootstrap.js

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -1,7 +1,36 @@
-<footer>
+<footer id="footer">
     <section class="container">
-        <div class="col-12">
-            <p class="text-center">jonasdamher Ⓒ 2022 - Todos los derechos reservados.</p>
+        <div class="row">
+            <div class="col-12">
+                <ul class="footer-social">
+                    <li>
+                        <a href="https://github.com/jonasdamher" target="_blank" rel="noreferrer" title="github">
+                            <img src="{{ asset('image/social/github.svg') }}" alt="github" width="24px" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/jonasdamher/" target="_blank" rel="noreferrer" title="linkedin">
+                            <img src="{{ asset('image/social/linkedin.svg') }}" alt="linkedin" width="24px" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://es.stackoverflow.com/users/161975/jonasdamher" target="_blank"
+                            rel="noreferrer" title="stack-overflow">
+                            <img src="{{ asset('image/social/stack-overflow.svg') }}" alt="stack-overflow"
+                                width="24px" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/jonasdamher" target="_blank" rel="noreferrer" title="twitter">
+                            <img src="{{ asset('image/social/twitter.svg') }}" alt="twitter" width="24px" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12">
+                <p class="text-center"><a href="{{ route('home') }}">jonasdamher</a> Ⓒ 2022 - Todos los derechos
+                    reservados.</p>
+            </div>
         </div>
     </section>
 </footer>
